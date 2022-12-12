6385183aaf08e7a4aa514eaedc0241a425895364
connorpn <connorpatricknolan@gmail.com>
2022-12-13 04:05:20 +1000

    x

--- notebooks and stata do files/Summary Stats/summary stats.py
+++ notebooks and stata do files/Summary Stats/summary stats.py
@@ -33,3 +33,26 @@ print('Set WD: Done')
 
 "IMPORT DATA"
 
+csr = pd.read_csv("C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/output/cross_sectional_returns_data.csv")
+ff = pd.read_csv ("C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/output/famafrench_factors.csv")
+carbon_beta = pd.read_csv ('C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/regression/regression outputs/carbon beta/carbon_beta_regression_output.csv')
+#%%
+
+csr_ff = pd.merge(csr, ff[['yearmonth','smb', 'hml', 'rmw', 'cma', 'wml']],how='outer',on=['yearmonth'])
+
+carbon_beta = carbon_beta.rename(columns={'_b_winsor_pmc': 'carbon_beta'})
+carbon_beta = carbon_beta[['carbon_beta']] 
+carbon_beta = carbon_beta.drop_duplicates(subset=['carbon_beta']).dropna().reset_index(drop=True)
+
+
+csr_ff_cb = csr_ff.join(carbon_beta)
+
+summ_stats = csr_ff_cb[['log_scope1', 'log_scope2', 'log_total_emissions', 'log_energy_consumption', 'change_scope1', 'change_scope2', 'change_total_emissions', 'change_energy_consumption', 'scope1_int', 'scope2_int', 'total_emissions_int', 'energy_consumption_int',
+                     'carbon_beta','ret','logsize', 'bm', 'leverage', 'mom', 'investa', 'roe', 'logppe', 'beta', 'volat', 'salesgr', 'epsgr',
+                     'smb', 'hml', 'rmw', 'cma', 'wml']]
+
+"Save summ_stats"
+output_filename = 'summ_stats.csv'
+output_path = 'C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/summary stats/'
+outputname = output_path + output_filename
+summ_stats.to_csv(outputname, mode='w', index=False)
