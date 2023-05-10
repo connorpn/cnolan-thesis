3993adfe898e2295a99b2e383fa6cf1b1f4be4cc
connorpn <connorpatricknolan@gmail.com>
2023-05-10 19:19:54 +1000

    x

--- notebooks and stata do files/Summary Stats/summary stats.do
+++ notebooks and stata do files/Summary Stats/summary stats.do
@@ -1,12 +1,27 @@
+/*
+ssc install estout
+ssc install winsor
+*/
+
 clear
 eststo clear
-ssc install logout
-
-import delimited "Z:\OneDrive\University Study\Honours Thesis\cnolan-thesis\summary stats\summ_stats.csv"
 
 
-destring log_scope2, replace force
-destring change_scope2, replace force
+  
+**************************		
+* Descriptive Statistics *
+**************************
+
+
+
+**************************		
+* BOLTON REPLICATION
+**************************
+/*
+*import data
+import delimited "Z:\OneDrive\University Study\Honours Thesis\cnolan-thesis\summary stats\br_summary_stats.csv"
+
+*winsorization
 
 winsor change_scope1, gen(winsor_change_scope1) p(0.025)
 winsor change_scope2, gen(winsor_change_scope2) p(0.025)
@@ -14,7 +29,6 @@ winsor change_total_emissions, gen(winsor_change_total_emissions) p(0.025)
 winsor change_energy_consumption, gen(winsor_change_energy_consumption) p(0.025)
 
 drop change_scope1 change_scope2 change_total_emissions change_energy_consumption
-
 rename winsor_change_scope1 change_scope1
 rename winsor_change_scope2 change_scope2
 rename winsor_change_total_emissions change_total_emissions
@@ -25,8 +39,7 @@ winsor scope2_int, gen(winsor_scope2_int) p(0.025)
 winsor total_emissions_int, gen(winsor_total_emissions_int) p(0.025)
 winsor energy_consumption_int, gen(winsor_energy_consumption_int) p(0.025)
 
-drop scope1_int scope2_int total_emissions_int energy_consumption
-
+drop scope1_int scope2_int total_emissions_int energy_consumption_int
 rename winsor_scope1_int scope1_int
 rename winsor_scope2_int scope2_int
 rename winsor_total_emissions_int total_emissions_int
@@ -41,9 +54,7 @@ winsor volat, gen(winsor_volat) p(0.005)
 winsor salesgr, gen(winsor_salesgr) p(0.005)
 winsor epsgr, gen(winsor_epsgr) p(0.005)
 
-
 drop bm leverage mom investa roe volat salesgr epsgr
-
 rename winsor_bm bm
 rename winsor_leverage leverage
 rename winsor_mom mom
@@ -53,34 +64,197 @@ rename winsor_volat volat
 rename winsor_salesgr salesgr
 rename winsor_epsgr epsgr
 
+
+eststo br_vars: estpost summ log_scope1 log_scope2 log_total_emissions log_energy_consumption change_scope1 change_scope2 change_total_emissions change_energy_consumption scope1_int scope2_int total_emissions_int energy_consumption_int ret logsize bm leverage mom investa roe logppe beta volat salesgr epsgr rmrf smb hml rmw cma wml, detail
+
+
 cd "Z:\OneDrive\University Study\Honours Thesis\cnolan-thesis\summary stats"
 
-logout, save("cb_summarize.tex") tex replace: tabstat log_scope1 log_scope2 log_total_emissions log_energy_consumption change_scope1 change_scope2 change_total_emissions change_energy_consumption scope1_int scope2_int total_emissions_int energy_consumption_int ret carbon_beta logsize bm leverage mom investa roe logppe beta volat salesgr epsgr, c(stat) stat(n mean sd min max p1 p5 p10 p25 p50 p75 p90 p95 p99 skewness kurtosis)
+#delimit ;
+esttab br_vars using "summ_stats_bolton.tex",
+cells("count mean(fmt(2)) sd(fmt(2))  p1(fmt(2)) p5(fmt(2)) p25(fmt(2)) p50(fmt(2)) p75(fmt(2)) p95(fmt(2)) p99(fmt(2))")
+nonumber nomtitle nonote noobs label booktabs
+collabels("N" "Mean" "SD"  "1" "5" "25" "Median" "75" "95" "99")
+refcat(log_scope1 "\newline{\textbf{Emissions Variables}}" ret "\newline{\textbf{Cross-sectional Variables}}" rmrf "\newline{\textbf{Time-series Variables}}", nolabel)
+coefl(
+log_scope1 "Log (Scope 1 Carbon Emissions)"
+log_scope2 "Log (Scope 2 Carbon Emissions)"
+log_total_emissions "Log (Total Carbon Emissions)"
+log_energy_consumption "Log (Energy Consumption)"
+change_scope1 "Change in Scope 1 Emissions"
+change_scope2 "Change in Scope 2 Emissions"
+change_total_emissions "Change in Total Emissions"
+change_energy_consumption "Change in Energy Consumption"
+scope1_int "Intensity of Scope 1 Emissions"
+scope2_int "Intensity of Scope 2 Emission"
+total_emissions_int "Intensity of Total Emission"
+energy_consumption_int "Intensity of Energy Consumption"
+ret "RET - Monthly Returns"
+logsize "LOGSIZE"
+bm "BM"
+leverage "LEVERAGE"
+mom "MOM"
+investa "INVEST/A"
+roe "ROE"
+logppe "LOGPPE"
+beta "BETA"
+volat "VOLAT"
+salesgr "SALESGR"
+epsgr "EPSGR"
+rmrf "MKTRF"
+smb "SMB"
+hml "HML"
+rmw "RMW"
+cma "CMA"
+wml "WML"
+)
+replace ;
+#delimit cr
+*/
 
+
+**************************		
+* CARBON BETA
+**************************
 /*
-foreach var of varlist _all {
-	label var `var' `var'
-}
+clear
+eststo clear
 
-estpost tabstat log_scope1 log_scope2 log_total_emissions log_energy_consumption change_scope1 change_scope2 change_total_emissions change_energy_consumption scope1_int scope2_int total_emissions_int energy_consumption_int ret carbon_beta logsize bm leverage mom investa roe logppe beta volat salesgr epsgr, c(stat) stat(sum mean sd min max n)
-
-*stat(n mean sd min max p1 p5 p10 p25 p50 p75 p90 p95 p99 skewness kurtosis)
-
-esttab using "cb_summarize.tex", replace ////
-cells("sum mean sd min max n")
-
-*collabels("Sum" "Mean" "SD" "Min" "Max" "N")
+*import data
+import delimited "Z:\OneDrive\University Study\Honours Thesis\cnolan-thesis\summary stats\pricing_carbon_risk_out.csv"
 
 
-esttab using "cb_summarize.tex", replace ////
- cells("n sum(fmt(%6.0fc)) mean(fmt(%6.2fc)) sd(fmt(%6.2fc)) min max p1 p5 p10 p25 p50 p75 p90 p95 p99 skewness kurtosis")   nonumber ///
-  nomtitle nonote noobs label booktabs ///
-  collabels("N" "Sum" "Mean" "SD" "Min" "Max" "p1" "p5" "p10" "p25" "p50" "p75" "p90" "p95" "p99" "skewness" "kurtosis")  ///
-  title("Summary Statistics \label{summarystats}")
+*winsorization
 
-  
-esttab using "cb_summarize.tex", replace ////
-cells("n sum(fmt(%6.0fc)) mean(fmt(%6.2fc)) sd(fmt(%6.2fc)) min max p1 p5 p10 p25 p50 p75 p90 p95 p99 skewness kurtosis")   nonumber ///
-nomtitle nonote noobs nolabel
+winsor ret, gen(winsor_ret) p(0.005)
+winsor ln_marketcap, gen(winsor_ln_marketcap) p(0.025)
+winsor bm, gen(winsor_bm) p(0.025)
+winsor roe, gen(winsor_roe) p(0.025) 
+winsor leverage, gen(winsor_leverage) p(0.025)
+winsor investa, gen(winsor_investa) p(0.025)
+winsor logppe, gen(winsor_logppe) p(0.025)
+winsor ppea, gen(winsor_ppea) p(0.025)
+winsor carbon_beta, gen(winsor_carbon_beta) p(0.025)
+winsor beta, gen(winsor_beta) p(0.005)
+winsor mom, gen(winsor_mom) p(0.005)
 
-  */
+
+drop ret ln_marketcap bm roe leverage investa logppe ppea carbon_beta beta mom
+rename winsor_ret ret
+rename winsor_ln_marketcap ln_marketcap
+rename winsor_bm bm
+rename winsor_roe roe
+rename winsor_leverage leverage
+rename winsor_investa investa
+rename winsor_logppe logppe
+rename winsor_ppea ppea
+rename winsor_carbon_beta carbon_beta
+rename winsor_beta beta
+rename winsor_mom mom
+
+*summary stats
+eststo cb_vars: estpost summ ret carbon_beta ln_marketcap bm roe leverage investa logppe ppea beta mom, detail
+
+*export to latex
+cd "Z:\OneDrive\University Study\Honours Thesis\cnolan-thesis\summary stats"
+
+#delimit ;
+esttab cb_vars using "summ_stats_carbon_beta.tex",
+cells("count mean(fmt(2)) sd(fmt(2))  p1(fmt(2)) p5(fmt(2)) p25(fmt(2)) p50(fmt(2)) p75(fmt(2)) p95(fmt(2)) p99(fmt(2))")
+nonumber nomtitle nonote noobs label booktabs
+collabels("N" "Mean" "SD"  "1" "5" "25" "Median" "75" "95" "99")
+coefl(
+ret "Monthly Returns"
+carbon_beta "Carbon Beta"
+ln_marketcap "ln(Market Cap.)"
+bm "Book/Market"
+roe "Return on Equity"
+leverage "Leverage"
+investa "Capex/Assets"
+logppe "ln(PPE)"
+ppea "PPE/Assets"
+beta "CAPM Beta"
+mom "Momentum"
+)
+replace ;
+#delimit cr
+*/
+
+**************************		
+* YEARLY FIRMS AND EMISSIONS
+**************************
+
+clear
+eststo clear
+
+*import data
+import delimited "Z:\OneDrive\University Study\Honours Thesis\cnolan-thesis\summary stats\yearly_stats.csv"
+
+*encode ticker
+label variable ticker "ticker"
+sort ticker
+encode ticker, gen(ticker_encode)
+
+*datetime 
+numdate yearly date = year, p(Y)
+
+*filter date
+drop if year < 2008
+drop if year > 2021
+
+*winsorization
+
+winsor ret, gen(winsor_ret) p(0.005)
+winsor ln_marketcap, gen(winsor_ln_marketcap) p(0.025)
+winsor bm, gen(winsor_bm) p(0.025)
+winsor roe, gen(winsor_roe) p(0.025) 
+winsor leverage, gen(winsor_leverage) p(0.025)
+winsor investa, gen(winsor_investa) p(0.025)
+winsor logppe, gen(winsor_logppe) p(0.025)
+winsor ppea, gen(winsor_ppea) p(0.025)
+winsor carbon_beta, gen(winsor_carbon_beta) p(0.025)
+winsor beta, gen(winsor_beta) p(0.005)
+winsor mom, gen(winsor_mom) p(0.005)
+
+
+drop ret ln_marketcap bm roe leverage investa logppe ppea carbon_beta beta mom
+rename winsor_ret ret
+rename winsor_ln_marketcap ln_marketcap
+rename winsor_bm bm
+rename winsor_roe roe
+rename winsor_leverage leverage
+rename winsor_investa investa
+rename winsor_logppe logppe
+rename winsor_ppea ppea
+rename winsor_carbon_beta carbon_beta
+rename winsor_beta beta
+rename winsor_mom mom
+
+*summary stats
+eststo cb_vars: estpost summ ret carbon_beta ln_marketcap bm roe leverage investa logppe ppea beta mom, detail
+
+*export to latex
+cd "Z:\OneDrive\University Study\Honours Thesis\cnolan-thesis\summary stats"
+
+#delimit ;
+esttab cb_vars using "summ_stats_carbon_beta.tex",
+cells("count mean(fmt(2)) sd(fmt(2))  p1(fmt(2)) p5(fmt(2)) p25(fmt(2)) p50(fmt(2)) p75(fmt(2)) p95(fmt(2)) p99(fmt(2))")
+nonumber nomtitle nonote noobs label booktabs
+collabels("N" "Mean" "SD"  "1" "5" "25" "Median" "75" "95" "99")
+coefl(
+ret "Monthly Returns"
+carbon_beta "Carbon Beta"
+ln_marketcap "ln(Market Cap.)"
+bm "Book/Market"
+roe "Return on Equity"
+leverage "Leverage"
+investa "Capex/Assets"
+logppe "ln(PPE)"
+ppea "PPE/Assets"
+beta "CAPM Beta"
+mom "Momentum"
+)
+replace ;
+#delimit cr
+*/
+
--- notebooks and stata do files/Summary Stats/summary stats.py
+++ notebooks and stata do files/Summary Stats/summary stats.py
@@ -31,28 +31,232 @@ output_path = working_directory +'./output/'
 print('Set WD: Done')
 #%%
 
+'''
+BOLTON REPLICATIONS
+'''
+
 "IMPORT DATA"
 
-csr = pd.read_csv("C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/output/cross_sectional_returns_data.csv")
-ff = pd.read_csv ("C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/output/famafrench_factors.csv")
-carbon_beta = pd.read_csv ('C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/regression/regression outputs/carbon beta/carbon_beta_regression_output.csv')
-#%%
-
-csr_ff = pd.merge(csr, ff[['yearmonth','smb', 'hml', 'rmw', 'cma', 'wml']],how='outer',on=['yearmonth'])
-
-carbon_beta = carbon_beta.rename(columns={'_b_winsor_pmc': 'carbon_beta'})
-carbon_beta = carbon_beta[['carbon_beta']] 
-carbon_beta = carbon_beta.drop_duplicates(subset=['carbon_beta']).dropna().reset_index(drop=True)
+csr = pd.read_csv('C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/regression/regression variables/br_carbon_emissions_and_stock_returns.csv')
+famafrench_factors = pd.read_csv ('C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/output/famafrench_factors.csv')
+rmrf = pd.read_csv("C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis./output/rmrf.csv")
 
 
-csr_ff_cb = csr_ff.join(carbon_beta)
+factors = pd.merge(rmrf[['yearmonth','rmrf']], famafrench_factors[['yearmonth','smb', 'hml', 'rmw', 'cma', 'wml']], how='inner', on=['yearmonth'])
+factors = factors.loc[(factors['yearmonth'] >= 200807) & (factors['yearmonth'] <= 202106)]
+factors = factors[['rmrf', 'smb', 'hml', 'rmw', 'cma', 'wml']]
 
-summ_stats = csr_ff_cb[['log_scope1', 'log_scope2', 'log_total_emissions', 'log_energy_consumption', 'change_scope1', 'change_scope2', 'change_total_emissions', 'change_energy_consumption', 'scope1_int', 'scope2_int', 'total_emissions_int', 'energy_consumption_int',
-                     'carbon_beta','ret','logsize', 'bm', 'leverage', 'mom', 'investa', 'roe', 'logppe', 'beta', 'volat', 'salesgr', 'epsgr',
-                     'smb', 'hml', 'rmw', 'cma', 'wml']]
+csr_br = csr
+csr_br = csr_br.loc[(csr_br['yearmonth'] >= 200807) & (csr_br['yearmonth'] <= 202106)]
+csr_br = csr_br[['ret', 'log_scope1', 'log_scope2', 'log_total_emissions', 'log_energy_consumption', 'change_scope1', 'change_scope2', 'change_total_emissions', 'change_energy_consumption', 'scope1_int', 'scope2_int', 'total_emissions_int', 'energy_consumption_int', 'logsize', 'bm', 'leverage', 'mom', 'investa', 'roe', 'logppe', 'beta', 'volat', 'salesgr', 'epsgr']]
 
-"Save summ_stats"
-output_filename = 'summ_stats.csv'
+br_summary_stats = pd.concat([csr_br, factors], axis="columns").reset_index(drop=True)
+
+"Save br_summary_stats"
+output_filename = 'br_summary_stats.csv'
 output_path = 'C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/summary stats/'
 outputname = output_path + output_filename
-summ_stats.to_csv(outputname, mode='w', index=False)
+br_summary_stats.to_csv(outputname, mode='w', index=False)
+print("Exported File: " + outputname)
+#%%
+'''
+CARBON BETA
+'''
+pricing_carbon_risk = pd.read_csv('C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/regression/regression variables/cb_asx500_pricing_carbon_risk.csv')
+#carbon_beta = pd.read_csv("C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/regression/regression outputs/carbon beta/carbon_beta_regression_output.csv")
+pricing_carbon_risk = pricing_carbon_risk.loc[(pricing_carbon_risk['yearmonth'] >= 200807) & (pricing_carbon_risk['yearmonth'] <= 202106)]
+pricing_carbon_risk_out = pricing_carbon_risk[['ret', 'ln_marketcap', 'bm', 'roe', 'leverage', 'investa', 'logppe', 'ppea', 'beta', 'mom', 'carbon_beta']].reset_index(drop=True)
+
+"Save pricing_carbon_risk_out"
+output_filename = 'pricing_carbon_risk_out.csv'
+output_path = 'C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/summary stats/'
+outputname = output_path + output_filename
+pricing_carbon_risk_out.to_csv(outputname, mode='w', index=False)
+print("Exported File: " + outputname)
+
+#%%
+'''
+#############YEARLY FIRM OBSERVATIONS
+'''
+#%%
+'''
+log emissions
+'''
+"import data"
+yearly_stats = pd.read_csv ("C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/regression/regression variables/br_determinants_of_carbon_emissions.csv")
+yearly_stats = yearly_stats[['year','ticker','log_scope1', 'log_scope2', 'log_total_emissions', 'log_energy_consumption', 'change_scope1', 'change_scope2', 'change_total_emissions', 'change_energy_consumption', 'scope1_int', 'scope2_int', 'total_emissions_int', 'energy_consumption_int']]
+yearly_stats_out = yearly_stats
+
+"Save yearly_stats_out"
+output_filename = 'yearly_stats_out.csv'
+output_path = 'C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/summary stats/'
+outputname = output_path + output_filename
+yearly_stats_out.to_csv(outputname, mode='w', index=False)
+print("Exported File: " + outputname)
+#%%
+
+'''
+EXP EMISSIONS
+'''
+
+nger_data_matched = pd.read_csv('C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/output/nger_data_matched.csv',encoding = "ISO-8859-1")
+matched_tickers = pd.read_csv("C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/output/matched_tickers.csv")
+matched_tickers_list = matched_tickers['ticker'].tolist()
+ms_data = pd.read_csv ('C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/output/ms_data.csv', encoding='latin1')
+
+"ADD TICKER YEAR ID"
+
+nger_data_matched['year'] = nger_data_matched['year'].astype(str)
+ms_data['year'] = ms_data['year'].astype(str)
+
+nger_data_matched['ticker_year'] = nger_data_matched['ticker'] + '_' + nger_data_matched['year']
+ms_data['ticker_year'] = ms_data['ticker'] + '_' + ms_data['year']
+
+
+"CONSTRUCT EMISSIONS VARIABLES"
+
+independent_vars_exp = nger_data_matched
+independent_vars_exp = pd.merge(independent_vars_exp, ms_data[['ticker_year','revenue']], how='left', on=['ticker_year'])
+independent_vars_exp = independent_vars_exp.replace([np.inf, -np.inf], np.nan)
+
+
+"YEAR BY YEAR CHANGE IN EMISSIONS"
+
+#clone each unique company for each observation year (2009-2021)
+firms_2009 = pd.DataFrame({'ticker':matched_tickers_list, 'year': '2009'})
+firms_2010 = pd.DataFrame({'ticker':matched_tickers_list, 'year': '2010'})
+firms_2011 = pd.DataFrame({'ticker':matched_tickers_list, 'year': '2011'})
+firms_2012 = pd.DataFrame({'ticker':matched_tickers_list, 'year': '2012'})
+firms_2013 = pd.DataFrame({'ticker':matched_tickers_list, 'year': '2013'})
+firms_2014 = pd.DataFrame({'ticker':matched_tickers_list, 'year': '2014'})
+firms_2015 = pd.DataFrame({'ticker':matched_tickers_list, 'year': '2015'})
+firms_2016 = pd.DataFrame({'ticker':matched_tickers_list, 'year': '2016'})
+firms_2017 = pd.DataFrame({'ticker':matched_tickers_list, 'year': '2017'})
+firms_2018 = pd.DataFrame({'ticker':matched_tickers_list, 'year': '2018'})
+firms_2019 = pd.DataFrame({'ticker':matched_tickers_list, 'year': '2019'})
+firms_2020 = pd.DataFrame({'ticker':matched_tickers_list, 'year': '2020'})
+firms_2021 = pd.DataFrame({'ticker':matched_tickers_list, 'year': '2021'})
+
+#create dataframe to add all cloned firm years
+firms_allyears = pd.DataFrame(columns = ['ticker', 'year'])
+
+#add all clone firm year dataframes to a list
+firm_years_list = [firms_2009, firms_2010, firms_2011, firms_2012, firms_2013, firms_2014, firms_2015, firms_2016, firms_2017, firms_2018, firms_2019, firms_2020, firms_2021]
+
+#concatenate dataframes
+firms_allyears = pd.concat(firm_years_list)
+
+firms_allyears['ticker_year'] = firms_allyears['ticker'] + '_' + firms_allyears['year']
+
+#create dummy level index
+independent_vars_exp["dummy_index"] = independent_vars_exp["dummy_index"] = 1
+firms_allyears['dummy_index'] = firms_allyears['dummy_index'] = 2
+
+#concenate cloned firm years with main data file
+independent_vars_exp = pd.concat([independent_vars_exp, firms_allyears]).reset_index(drop=True)
+
+#sort dataframe by corporation name and year
+independent_vars_exp = independent_vars_exp.sort_values(by=['ticker', 'year']).reset_index(drop=True)
+
+#drop duplicates keeping first row (main  data file) as by dummy_index sorting
+independent_vars_exp = independent_vars_exp.drop_duplicates(['ticker', 'year',], keep='first').reset_index(drop=True)
+
+
+#independent_vars_exp['year'] =  pd.to_datetime(independent_vars_exp['year'], format='%Y').dt.to_period("Y")
+
+independent_vars_exp = independent_vars_exp.sort_values(by=['ticker','year'])
+
+#calculate yearly change in emissions by corporation for scope1, scope2, total_emissions, and energy consumption
+
+
+#on log emissions
+
+independent_vars_exp = independent_vars_exp.sort_values(by=['ticker','year'])
+independent_vars_exp['change_scope1'] = independent_vars_exp.groupby(['ticker'])['scope1'].diff()
+independent_vars_exp['change_scope2'] = independent_vars_exp.groupby(['ticker'])['scope2'].diff()
+independent_vars_exp['change_total_emissions'] = independent_vars_exp.groupby(['ticker'])['total_emissions'].diff()
+independent_vars_exp['change_energy_consumption'] = independent_vars_exp.groupby(['ticker'])['energy_consumption'].diff()
+
+independent_vars_exp = independent_vars_exp.replace([np.inf, -np.inf], np.nan).reset_index(drop=True)
+
+
+"EMISSIONS INTENSITY"
+
+'''
+## int = (tons CO 2 e/AUD m.)
+independent_vars_exp['revenue(m)'] = independent_vars_exp['revenue'] / 1000000
+
+independent_vars_exp['scope1_int'] = independent_vars_exp['scope1'] /  independent_vars_exp['revenue(m)']
+independent_vars_exp['scope2_int'] = independent_vars_exp['scope2'] /  independent_vars_exp['revenue(m)']
+independent_vars_exp['total_emissions_int'] = independent_vars_exp['total_emissions'] /  independent_vars_exp['revenue(m)']
+independent_vars_exp['energy_consumption_int'] = independent_vars_exp['energy_consumption'] / independent_vars_exp['revenue(m)']
+
+independent_vars_exp['scope1_int'] = independent_vars_exp['scope1_int']/100
+independent_vars_exp['scope2_int'] = independent_vars_exp['scope2_int']/100
+independent_vars_exp['total_emissions_int'] = independent_vars_exp['total_emissions_int']/100
+independent_vars_exp['energy_consumption_int'] = independent_vars_exp['energy_consumption_int']/100
+'''
+
+
+"merge intensity"
+independent_vars_exp['year'] = independent_vars_exp['year'].astype(int)
+independent_vars_exp = pd.merge(independent_vars_exp, yearly_stats[['year', 'ticker', 'scope1_int', 'scope2_int', 'total_emissions_int', 'energy_consumption_int']], how='left', on=['year','ticker'])
+
+"DROP BLANK OBSERVATIONS"
+independent_vars_exp = independent_vars_exp.dropna(subset=['scope1', 'scope2', 'total_emissions', 'energy_consumption', 'change_scope1', 'change_scope2', 'change_total_emissions', 'change_energy_consumption', 'scope1_int', 'scope2_int', 'total_emissions_int', 'energy_consumption_int'],how='all').reset_index(drop=True)
+independent_vars_exp = independent_vars_exp[['year', 'ticker', 'ticker_year', 'scope1', 'scope2', 'total_emissions', 'energy_consumption', 'change_scope1', 'change_scope2', 'change_total_emissions', 'change_energy_consumption', 'scope1_int', 'scope2_int', 'total_emissions_int', 'energy_consumption_int']]
+
+yearly_stats_exp_out = independent_vars_exp
+
+"Save yearly_stats_exp_out"
+output_filename = 'yearly_stats_exp_out.csv'
+output_path = 'C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/summary stats/'
+outputname = output_path + output_filename
+yearly_stats_exp_out.to_csv(outputname, mode='w', index=False)
+print("Exported File: " + outputname)
+
+
+
+#%%
+
+"GROUP BY AGG"
+
+"YEAERLY STATS TAB"
+yearly_stats_out_winsor = pd.read_csv('C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/summary stats/yearly_stats_out_winsor.csv')
+yearly_stats_log_agg = yearly_stats_out_winsor.groupby("year").agg({"ticker": pd.Series.nunique, 'log_scope1': np.mean, 'log_scope2': np.mean, 'log_total_emissions': np.mean, 'log_energy_consumption': np.mean, 'change_scope1': np.mean, 'change_scope2': np.mean, 'change_total_emissions': np.mean, 'change_energy_consumption': np.mean, 'scope1_int': np.mean, 'scope2_int': np.mean, 'total_emissions_int': np.mean, 'energy_consumption_int': np.mean,})
+yearly_stats_log_agg =yearly_stats_log_agg.assign(table="log")
+
+"Save yearly_stats_log_agg"
+output_filename = 'yearly_stats_log_agg.csv'
+output_path = 'C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/summary stats/'
+outputname = output_path + output_filename
+yearly_stats_log_agg.to_csv(outputname, mode='w', index=False)
+print("Exported File: " + outputname)
+
+"YEARLY STATS TAB E"
+yearly_stats_exp_out_winsor = pd.read_csv('C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/summary stats/yearly_stats_exp_out_winsor.csv')
+yearly_stats_exp_agg = yearly_stats_exp_out_winsor.groupby("year").agg({"ticker": pd.Series.nunique, 'scope1': np.mean, 'scope2': np.mean, 'total_emissions': np.mean, 'energy_consumption': np.mean, 'change_scope1': np.mean, 'change_scope2': np.mean, 'change_total_emissions': np.mean, 'change_energy_consumption': np.mean, 'scope1_int': np.mean, 'scope2_int': np.mean, 'total_emissions_int': np.mean, 'energy_consumption_int': np.mean})
+yearly_stats_exp_agg =yearly_stats_exp_agg.assign(table="exp")
+
+"Save yearly_stats_exp_agg"
+output_filename = 'yearly_stats_exp_agg.csv'
+output_path = 'C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/summary stats/'
+outputname = output_path + output_filename
+yearly_stats_exp_agg.to_csv(outputname, mode='w', index=False)
+print("Exported File: " + outputname)
+
+
+"YEARLY STATS CONACT"
+yearly_stats_exp_agg = yearly_stats_exp_agg.rename(columns={'scope1': 'log_scope1', 'scope2':'log_scope2', 'total_emissions': 'log_total_emissions', 'energy_consumption': 'log_energy_consumption'})
+
+yearly_stats_concat_agg = yearly_stats_log_agg.append(yearly_stats_exp_agg)
+yearly_stats_concat_agg = yearly_stats_concat_agg.reset_index()
+yearly_stats_concat_agg['year'] = yearly_stats_concat_agg['year'].astype(int)
+
+
+"Save yearly_stats_concat_agg"
+output_filename = 'yearly_stats_concat_agg.csv'
+output_path = 'C:/Users/conno/OneDrive/University Study/Honours Thesis/cnolan-thesis/summary stats/'
+outputname = output_path + output_filename
+yearly_stats_concat_agg.to_csv(outputname, mode='w', index=False)
+print("Exported File: " + outputname)
